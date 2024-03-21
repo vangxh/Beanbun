e3e96f400a23eda6c501d3d0a001432fd3593361
鬼手 <vangxh@qq.com>
2024-03-21 20:02:03 +0800

    Update MemoryQueue.php

--- src/Queue/MemoryQueue.php
+++ src/Queue/MemoryQueue.php
@@ -131,7 +131,7 @@ class MemoryQueue implements QueueInterface
     public function isQueued($queue)
     {
         if ($this->bloomFilter) {
-            return $this->globalData->bfIn($this->queuedKey, md5($queue));
+            return $this->globalData->bfIn($this->queuedKey, md5(serialize($queue)));
         } else {
             return in_array($queue, $this->globalData->{$this->queuedKey});
         }
